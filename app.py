71cddef67c2343ee16141ca0d9556fde06323af5
Crystal Kishore <crystalkishore7@gmail.com>
2025-08-04 07:59:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!!"
 
+
 @app.route("/sms", methods=['POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
@@ -47,3 +48,4 @@ def sms_reply():
  
 if __name__ == "__main__":
     app.run(debug=True)
+
