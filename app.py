7abbb54533958817f40bfec7cb847636980791d5
Crystal Kishore <crystalkishore7@gmail.com>
2025-08-13 22:46:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!!"
+    return "Hello, World!!!"
 
 
 @app.route("/sms", methods=['POST'])
@@ -48,5 +48,6 @@ def sms_reply():
  
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
